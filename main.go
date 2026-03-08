3549179b5394c63de063dc154c671faa51e24d93
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2026-03-08 20:06:06 +0530

    Adding a Logger

--- main.go
+++ main.go
@@ -3,22 +3,34 @@ package main
 import (
 	"log"
 	"net/http"
+	"rahulkumarpahwa.me/go-and-js-fullstack/logger"
 )
 
+func initialiseLogger() *logger.Logger {
+	logInstance, err := logger.NewLogger("movie-service.log")
+	if err != nil {
+		log.Fatalf("Failed to initialise logger : %v", err)
+	}
+	return logInstance
+}
+
 func main() {
+	var logInstance *logger.Logger = initialiseLogger()
+
 	const address string = ":8080" // we will put this later in the env file.
 
 	server := http.NewServeMux()
 
 	// we are serving the static files such as the images, css and json file.
 	server.Handle("/", http.FileServer(http.Dir("./public")))
-	server.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello"))
-	})
 
+
+
+	logInstance.Info("Server Starting on port " + address)
 	err := http.ListenAndServe(address, server)
 
 	if err != nil {
+		logInstance.Error("Server failed to start ", err )
 		log.Fatalf("Server Failed : %v", err)
 	}
 
