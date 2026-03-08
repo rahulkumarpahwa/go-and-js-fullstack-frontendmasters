c162612037d7d87d1373b9351d9c16d815db0f38
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2026-03-08 22:06:30 +0530

    till lecture 8: Creating a JSON Route Handler

--- models/movie.go
+++ models/movie.go
@@ -2,11 +2,11 @@ package models
 
 type Movie struct {
 	ID          int
-	TMBD_ID     int
+	TMDB_ID     int
 	Title       string
 	Tagline     string
 	ReleaseYear int
-	Genre       []Genre
+	Genres       []Genre
 	Score       *float32
 	Popularity  *float32
 	Keywords    []string
