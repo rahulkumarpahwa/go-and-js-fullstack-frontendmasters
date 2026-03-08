4269e15424678ed939b9a536d35739b821135bf9
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2026-03-08 09:24:39 +0530

    till lecture 4 : simple server setup

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
+	const address string = ":8080" // we will put this later in the env file.
+
+	server := http.NewServeMux()
+
+	// we are serving the static files such as the images, css and json file.
+	server.Handle("/", http.FileServer(http.Dir("./public")))
+
+	err := http.ListenAndServe(address, server)
+
+	if err != nil {
+		log.Fatalf("Server Failed : %v", err)
+	}
 
 }
