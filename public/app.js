5aa2a4a7e9c6038e9cfd9447dd8411ef7401aae1
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2026-03-12 06:11:56 +0530

    till lecture 21 : Creating App.js

---
+++ public/app.js
@@ -0,0 +1,7 @@
+window.app = {
+  search: (event) => {
+    event.preventDefault();
+    const q = document.querySelector("input[type=search]").value;
+    // TODO, make the api call here.
+  },
+};
