e7ff505b38cded7fdd4df743a4782946cd97e7a1
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2026-03-12 21:26:18 +0530

    till lecture 25 : Dynamically Render Component

--- public/components/HomePage.js
+++ public/components/HomePage.js
@@ -1,13 +1,46 @@
+import { API } from "../services/api.js"; // always add the .js at the end.
+
 export class HomePage extends HTMLElement {
   constructor() {
     super(); // must call this
   }
 
+  
+
+  // custom method not the one given by the HTMLElement class
+  async render() {
+    const topMovies = await API.getTopMovies();
+    renderMoviesInList(
+      topMovies,
+      document.getElementById("top-10").children[1],
+    );
+
+    const randomMovies = await API.getRandomMovies();
+    renderMoviesInList(
+      randomMovies,
+      document.getElementById("random").children[1],
+    );
+
+    function renderMoviesInList(movies, ul) {
+      ul.innerHTML = "";
+      movies.forEach((obj) => {
+        const li = document.createElement("li");
+        li.innerHTML = `<div>
+                    <img src="${obj.poster_url}" alt="${obj.title}"/>
+                    <p>${obj.title}</p>
+        </div>`;
+        ul.appendChild(li);
+      });
+    }
+  }
+
   // this method will be called when element (here home-page) is pushed into the dom, then will be called. It is like the event, like mounted event over our element.
+  // Given by HTMLElement class, which we need to override.
   connectedCallback() {
     const template = document.getElementById("template-home");
     const content = template.content.cloneNode(true);
     this.appendChild(content);
+    this.render();
   }
 }
 
