024574c49811547c37aa5d9e36cb8c22e666f702
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2026-03-12 06:55:26 +0530

    till lecture 22 : Client Side API Service

--- public/app.js
+++ public/app.js
@@ -1,7 +1,9 @@
+import { API } from "./services/api.js";
 window.app = {
   search: (event) => {
     event.preventDefault();
-    const q = document.querySelector("input[type=search]").value;
+    const q = document.querySelector("input[type='search']").value;
     // TODO, make the api call here.
   },
+  api: API,
 };
---
+++ public/services/api.js
@@ -0,0 +1,30 @@
+export const API = {
+  baseURL: "/api/",
+
+  getTopMovies: async () => {
+    return await API.fetch("movies/top");
+  },
+  getRandomMovies: async () => {
+    return await API.fetch("movies/random");
+  },
+  getMovieById: async (id) => {
+    return await API.fetch(`movies/${id}`);
+  },
+  searchMovies: async (name, order, genre) => {
+    return await API.fetch(`movies/search`, { name, order, genre });
+  },
+  getGenres: async () => {
+    return await API.fetch("genres");
+  },
+
+  fetch: async (ServiceWorker, args) => {
+    try {
+      const queryString = args ? new URLSearchParams(args).toString : "";
+      const response = await fetch(API.baseURL + ServiceWorker + queryString);
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
