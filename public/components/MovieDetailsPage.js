52b2ad0fb282077a7c24065eab853a9bd85e9ea4
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2026-03-15 13:51:54 +0530

    till lecture 30 : Embedding Elements in Movie Details

--- public/components/MovieDetailsPage.js
+++ public/components/MovieDetailsPage.js
@@ -23,9 +23,36 @@ export class MovieDetailsPage extends HTMLElement {
     this.appendChild(content);
 
     this.querySelector("h2").textContent = this.movie.title;
-      this.querySelector("h3").textContent = this.movie.tagline;
+    this.querySelector("h3").textContent = this.movie.tagline;
+    this.querySelector("img").src = this.movie.poster_url;
+    this.querySelector("#overview").textContent = this.movie.overview;
+    this.querySelector("#metadata").innerHTML = `
+    <dt>Release Year</dt>
+    <dd>${this.movie.release_year}</dd>
+    <dt>Score</dt>
+    <dd>${this.movie.score} / 10</dd>
+    <dt>Popularity</dt>
+    <dd>${this.movie.popularity.toFixed(2)}</dd>
+    `;
 
+    const ulGenres = this.querySelector("#genres");
+    ulGenres.innerHTML = "";
+    this.movie.genres.forEach((genre) => {
+      const li = document.createElement("li");
+      li.textContent = genre.name;
+      ulGenres.appendChild(li);
+    });
 
+    const ulCast = this.querySelector("#cast");
+    ulCast.innerHTML = "";
+    this.movie.casting.forEach((actor) => {
+      const li = document.createElement("li");
+      li.innerHTML = `
+      <img src="${actor.image_url ?? "/images/generic_actor.jpg"}" alt="Picture of  ${actor.first_name} ${actor.last_name}" />
+      <p>${actor.first_name} ${actor.last_name}</p>
+      `;
+      ulCast.appendChild(li);
+    });
   }
 
   //NOTE : this method will called once the code is parsed and mounted and WILL NOT BE CALLED AGAIN WHEN THE ATTRIBUTE CHANGES.
