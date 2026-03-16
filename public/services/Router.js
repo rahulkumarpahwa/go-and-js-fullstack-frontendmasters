f051538df9f556bb39b347862c651ffafe4c74a5
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2026-03-16 08:38:07 +0530

    till lecture 34 : Creating a Generic Router

--- public/services/Router.js
+++ public/services/Router.js
@@ -1,3 +1,4 @@
+import { routes } from "./Routes.js";
 export const Router = {
   init: () => {
     window.addEventListener("popstate", () => {
@@ -10,6 +11,31 @@ export const Router = {
   go: (route, addToHistory = true) => {
     if (addToHistory) {
       history.pushState(null, "", route);
+    }
+
+    let pageElement = null;
+
+    const routePath = route.includes("?") ? route.split("?")[0] : route;
+
+    for (const r of routes) {
+      if (typeof r.path == "string" && r === routePath) {
+        pageElement = new r.component();
+      } else if (r.path instanceof RegExp) {
+        const match = r.path.exec(route);
+        if (match) {
+          pageElement = new r.component();
+          const params = match.slice(1);
+          pageElement.params = params;
+        }
+      }
+    }
+
+    if (pageElement == null) {
+      pageElement = document.createElement("h1");
+      pageElement.textContent = "Page Not Found!";
+    } else {
+      document.querySelector("main").innerHTML = "";
+      document.querySelector("main").appendChild(pageElement);
     }
   },
 };
