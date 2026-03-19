ed62ba35260e0deb108ed42bdcf4a303a7f465e6
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2026-03-19 20:16:09 +0530

    till lecture 44: Adding Authentication Storage

--- models/user.go
+++ models/user.go
@@ -1,5 +1,10 @@
 package models
 
-type User struct{
-	
+type User struct {
+	ID        int    `json:"id"`
+	Name      string `json:"name"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	Favorites []Movie
+	Watchlist []Movie
 }
