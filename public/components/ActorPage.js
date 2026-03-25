0a2e4279b52126ac3a85ca11eb05c3fa4675c26f
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2026-03-25 09:35:46 +0530

    Refactor ActorPage component to integrate API for actor details and update movie list rendering; rename movie result list ID for clarity

--- public/components/ActorPage.js
+++ public/components/ActorPage.js
@@ -1,21 +1,40 @@
+import { API } from "../services/api.js";
+import { MovieItem } from "./MovieItem.js";
+
 export class ActorPage extends HTMLElement {
   constructor() {
     super();
+    this.id = null;
   }
 
-  async render(){
-    
-  }
+  async render() {
+    const data = API.getActorDetails(this.id);
+    const actor = data?.actor;
+    const movies = data?.movies ?? [];
 
+    const div = document.querySelector("#actor-result");
+    div.innerHTML 
+
+    const ul = document.querySelector("#actor-movies-result");
+
+    if (movies && movies.length > 0) {
+      ul.innerHTML = "";
+      movies.forEach((movie) => {
+        const li = document.createElement("li");
+        li.appendChild(new MovieItem(movie));
+      });
+      this.appendChild(ul);
+    }
+  }
 
   connectedCallback() {
-    const template = document.getElementById("template-actor-details")
+    const template = document.getElementById("template-actor-details");
     const content = template.content.cloneNode(true);
 
     this.appendChild(content);
 
     this.render();
-
+    this.id = this.params[0];
   }
 }
 
